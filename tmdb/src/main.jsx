d71e40de5535d5194c3794b84763d251ba8cb48d
Emma <emmandersson166@gmail.com>
2021-09-08 15:58:43 +0200

    react query + router wrapper

--- tmdb/src/main.jsx
+++ tmdb/src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import "./index.css";
+import App from "./App";
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
