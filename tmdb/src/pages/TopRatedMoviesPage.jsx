77b77ca1a22d5bbe33806b33ce487ecb4ac6809c
Emma <emmandersson166@gmail.com>
2021-09-08 16:00:51 +0200

    created topRatedMovies component

---
+++ tmdb/src/pages/TopRatedMoviesPage.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getTopRatedMovies } from "../services/API";
+
+const TopRatedMoviesPage = () => {
+  const history = useHistory();
+
+  const { data, error, isError, isLoading } = useQuery(
+    "top-rated-movies",
+    getTopRatedMovies
+  );
+
+  const handleClickToMovieId = (movieId) => {
+    history.push(`/movies/${movieId}`);
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <>
+      {isLoading && <p>Loading movies...</p>}
+      {isError && <p>({error})</p>}
+      {data?.results && (
+        <main>
+          {data.results.map((movie, i) => (
+            <p onClick={() => handleClickToMovieId(movie.id)} key={movie.id}>
+              {i + 1}. {movie.title}
+            </p>
+          ))}
+        </main>
+      )}
+    </>
+  );
+};
+
+export default TopRatedMoviesPage;
