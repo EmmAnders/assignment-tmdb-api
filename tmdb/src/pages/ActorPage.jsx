fad5cfbe6b06297a473678097469938823de012f
Emma <emmandersson166@gmail.com>
2021-09-08 16:00:25 +0200

    created actorPage component

---
+++ tmdb/src/pages/ActorPage.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getMovieDetails, getCastByMovieId } from "../services/API";
+
+const ActorPage = () => {
+  return <div>Actor</div>;
+};
+
+export default ActorPage;
