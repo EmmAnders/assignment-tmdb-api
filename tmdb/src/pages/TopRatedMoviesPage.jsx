430e9db933ea1685377f6cea57c9372754437cd9
Emma <emmandersson166@gmail.com>
2021-09-10 15:08:00 +0200

    toprated movies page done

--- tmdb/src/pages/TopRatedMoviesPage.jsx
+++ tmdb/src/pages/TopRatedMoviesPage.jsx
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom";
 import { useQuery } from "react-query";
 import { getTopRatedMovies } from "../services/API";
 
+import Card from "../components/Card";
+import "../scss/pages/Movies.scss";
 
 const TopRatedMoviesPage = () => {
   const history = useHistory();
@@ -17,22 +19,35 @@ const TopRatedMoviesPage = () => {
     window.scrollTo(0, 0);
   };
 
-  {data && (console.log(data))}
-
   return (
     <>
       {isLoading && <p>Loading movies...</p>}
       {isError && <p>({error})</p>}
       {data?.results && (
-        <main>
-          {data.results.map((movie, i) => (
-            <p onClick={() => handleClickToMovieId(movie.id)} key={movie.id}>
-              {i + 1}. {movie.title}
-            </p>
-          ))}
+        <main className="page-wrapper">
+          <section className="header">
+            <h1>Top Rated Movies</h1>
+          </section>
+          <section className="content">
+            {data.results.map((movie, i) => (
+              <div
+                onClick={() => handleClickToMovieId(movie.id)}
+                key={movie.id}
+              >
+                <Card
+                  src={movie.poster_path}
+                  title={movie.title}
+                  /*   originalTitle={movie.original_title}
+                  originalLanguage={movie.original_language} */
+                  releaseDate={movie.release_date}
+                  voteAverage={movie.vote_average}
+                  genre1={movie.genre_ids.map((id) => id).join("/")}
+                ></Card>
+              </div>
+            ))}
+          </section>
         </main>
       )}
-     
     </>
   );
 };
