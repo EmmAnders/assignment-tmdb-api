3fbf9075117da6382c92bafb52433139811a4a71
Emma <emmandersson166@gmail.com>
2021-09-14 18:32:57 +0200

    clean up in jsx

--- tmdb/src/components/Card.jsx
+++ tmdb/src/components/Card.jsx
@@ -1,36 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "../scss/components/Card.scss";
-import movie from "../assets/icons/movie.svg";
-import arrow from "../assets/icons/arrow-down-right.svg";
+
 
 const Card = (props) => {
   const baseUrlImg = "https://image.tmdb.org/t/p/w500";
 
   return (
-    <div className="card">
-      <div className="img">
+    <div onClick={props.onClick} className="card">
+      <div className="card-img">
         <img src={baseUrlImg + props.src} alt={props.alt} />
       </div>
-      <div className="genres">
-        <p>{props.genre1}</p>
-        <p>{props.genre2}</p>
-        <p>{props.genre3}</p>
-        <p>{props.genre4}</p>
-      </div>
-      <div className="title">
+
+      <div className="card-title">
         <h2>{props.title}</h2>
       </div>
-      <div className="hover-content">
-        {/* <div className="original-language">
-          <p className="sub-title">Original Language</p>
-          <p>{props.originalLanguage}</p>
-        </div>
 
-        <div className="original-title">
-          <p className="sub-title">Original Title:</p>
-          <p>{props.originalTitle}</p>
-        </div> */}
+      <div className="hover-content">
         <div className="details-wrapper">
           <div className="release-date">
             <p className="sub-title">Release Date</p>
@@ -41,16 +27,6 @@ const Card = (props) => {
             <p className="sub-title">{props.subtitle}</p>
             <p>{props.voteAverage}</p>
           </div>
-        </div>
-
-        <div className="cta-wrapper">
-          <div className="cta">
-            <p>View details </p>
-            <img src={arrow} alt="arrow" />
-          </div>
-          {/*    <div>
-            <img src={movie} alt="movie icon" />
-          </div> */}
         </div>
       </div>
     </div>
